eeda0efa4945050e6e73845516a7a51b10916bb8
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2023-09-24 20:05:36 +0530

    fix: #2056

--- src/components/widgets/resources/disk.jsx
+++ src/components/widgets/resources/disk.jsx
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import useSWR from "swr";
 import { FiHardDrive } from "react-icons/fi";
 import { useTranslation } from "next-i18next";
@@ -16,28 +17,28 @@ export default function Disk({ options, expanded, refresh = 1500 }) {
     return <Error options={options} />
   }
 
-  if (!data) {
+  if (_.isEmpty(data)) {
     return <Resource
-      icon={FiHardDrive}
-      value="-"
-      label={t("resources.free")}
-      expandedValue="-"
-      expandedLabel={t("resources.total")}
-      expanded={expanded}
-      percentage="0"
-    />;
+        icon={FiHardDrive}
+        value="-"
+        label={t("resources.free")}
+        expandedValue="-"
+        expandedLabel={t("resources.total")}
+        expanded={expanded}
+        percentage="0"
+      />;
   }
 
   // data.drive.used not accurate?
   const percent = Math.round(((data.drive.size - data.drive.available) / data.drive.size) * 100);
 
   return <Resource
-    icon={FiHardDrive}
-    value={t("common.bytes", { value: data.drive.available })}
-    label={t("resources.free")}
-    expandedValue={t("common.bytes", { value: data.drive.size })}
-    expandedLabel={t("resources.total")}
-    percentage={percent}
-    expanded={expanded}
-  />;
+      icon={FiHardDrive}
+      value={t("common.bytes", { value: data.drive.available })}
+      label={t("resources.free")}
+      expandedValue={t("common.bytes", { value: data.drive.size })}
+      expandedLabel={t("resources.total")}
+      percentage={percent}
+      expanded={expanded}
+    />;
 }
